02bfe53c09ff80067f53e068f98653ab33f46f23
Yuratama1 <yuratama661@gmail.com>
2025-06-03 18:37:46 +0700

    membuat class pelajar dengan keturunannya

--- solvingmultiwaris.cpp
+++ solvingmultiwaris.cpp
@@ -17,3 +17,11 @@ class pekerja : virtual public orang {
                 cout << "pekerja dibuat\n" << endl;
             }
 };
+
+class pelajar : virtual public orang {
+    public:
+        pelajar(int pUmur) :
+            orang(pUmur){
+                cout << "pelajar dibuat\n" << endl;
+            }
+};
