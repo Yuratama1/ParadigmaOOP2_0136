1b2126e1d51f2b1be929c7ef8328ba1c18d8131d
Yuratama1 <yuratama661@gmail.com>
2025-06-03 18:45:05 +0700

    membuat class derived class

--- noOverrriding.cpp
+++ noOverrriding.cpp
@@ -7,3 +7,10 @@ class baseClass{
             cout << "hallo saya fungsi dari base class";
         }
 };
+
+class derivedclass : public baseClass{
+    public:
+        void perkenalan(){
+            cout << "hallo saya fungsi dari derived class";
+        }
+};
