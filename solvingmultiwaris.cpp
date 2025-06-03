20b5c7456b94e4335cc55082f4fe68cbe58e8fce
Yuratama1 <yuratama661@gmail.com>
2025-06-03 18:41:52 +0700

    finish

--- solvingmultiwaris.cpp
+++ solvingmultiwaris.cpp
@@ -20,8 +20,22 @@ class pekerja : virtual public orang {
 
 class pelajar : virtual public orang {
     public:
-        pelajar(int pUmur) :
-            orang(pUmur){
+        pelajar(int pumur) :
+            orang(pumur){
                 cout << "pelajar dibuat\n" << endl;
             }
 };
+
+class budi : public pekerja, public pelajar{
+    public:
+        budi(int pumur) :
+        pekerja(pumur),
+        pelajar(pumur) {
+            cout << "Budi dibuat\n" << endl;
+        }
+};
+
+int main (){
+    budi a(12);
+    return 0;
+}
